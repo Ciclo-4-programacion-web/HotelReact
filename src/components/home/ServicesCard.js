8c821b22f11cfba0229a970ab5c89c2ae0c8c527
Julian Montero <71648137+JulianMGonzalez@users.noreply.github.com>
2021-07-31 18:18:42 -0500

    first commit

---
+++ src/components/home/ServicesCard.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const ServicesCard = ({ name, link, description, author, avatar }) => {
+    return (
+        <div className='mx-4'>
+            <div className="max-w-2xl px-8 py-6 mx-auto bg-white rounded-lg shadow-lg">
+                <div className="flex items-center">
+                    <img className="w-10 h-10 rounded-full mr-2" src={avatar} alt="Avatar of Writer" />
+                    <div className="text-sm">
+                        <p className="text-gray-900 leading-none">{author}</p>
+                    </div>
+                </div>
+                <div className="mt-2">
+                    <h2 href="#3" className="text-2xl font-bold text-gray-700">{name}</h2>
+                    <p className="mt-2 text-gray-600">{description}</p>
+                </div>
+                <div className="mt-4"><a href={link}
+                    className="text-blue-500 text-center hover:underline">Leer mas</a>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ServicesCard
---
+++ src/pages/public/Home.js
@@ -0,0 +1,39 @@
+import FooterComponent from 'components/FooterComponent'
+import RoomReview from 'components/home/RoomReview'
+import ServicesCard from 'components/home/ServicesCard'
+import React from 'react'
+
+export const Home = () => {
+    return (
+        <div>
+            <div>
+                <div className="bg-cover bg-center h-full text-white py-40 px-10 object-fill" style={{ backgroundImage: `url("https://media.tdc.travel/tdc_media/tmp/new/1476998887.1400.banner-hotel.jpg")` }}>
+                    <div className="flex flex-col items-center" >
+                        <p className="text-3xl font-bold">Hotel reservas</p>
+                        <a href="#2" className="bg-blue-800 py-4 px-8 text-white font-bold uppercase text-xs rounded hover:bg-blue-500 hover:text-white">Servicios</a>
+                    </div>
+                </div>
+                <div className='bottom-0 flex mx-auto my-10'>
+                </div>
+                <div className='flex flex-wrap justify-center my-10'>
+
+                    <ServicesCard
+                        name='Hotel React'
+                        link='#4'
+                        description='El HotelReact es un moderno y elegante hotel de 4 estrellas, asomado al mar, ideal para unas vacaciones románticas y de gran encanto, en el mágico escenario de Taormina y del Mar de Sicilia.'
+                        author='Julian Montero'
+                        avatar='https://cdn.pixabay.com/photo/2018/08/28/12/41/avatar-3637425_640.png'
+                    />
+                    <ServicesCard
+                        name='Mejores Habitaciones!'
+                        link='#4'
+                        description='El HotelReact es un moderno y elegante hotel de 4 estrellas, asomado al mar, ideal para unas vacaciones románticas y de gran encanto, en el mágico escenario de Taormina y del Mar de Sicilia.'
+                        author='Julian Montero'
+                        avatar='https://cdn.pixabay.com/photo/2018/08/28/12/41/avatar-3637425_640.png' />
+                </div>
+                <RoomReview/>
+                <FooterComponent/>
+            </div>
+        </div>
+    )
+}
